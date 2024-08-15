b6bc2a08d66270faf429a3e9dc01a0bef61d4a23
Ammar Al-Ahdab <amar39771@gmail.com>
2024-08-14 17:16:58 -0700

    change BASE url of API for Render Server

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,7 +4,7 @@ import axios from "axios";
 // const BASE_URL = "http://127.0.0.1:8000/api";
 
 // FOR SERVER
-const BASE_URL = "https//nogaproject.onrender.com";
+const BASE_URL = "https://nogaproject.onrender.com/api";
 
 export default axios.create({
   baseURL: BASE_URL,
