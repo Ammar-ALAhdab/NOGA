1a0473f89042eceee9d75b21deca42d2b0270ee5
Ammar Al-Ahdab <amar39771@gmail.com>
2024-08-15 15:32:09 -0700

    fix locale api

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,16 +1,15 @@
 import axios from "axios";
 
 // FOR LOCAL
-// const BASE_URL = "http://127.0.0.1:8000/api";
+const BASE_URL = "http://127.0.0.1:8000/api";
 
 // FOR SERVER
-const BASE_URL = "https://nogaproject.onrender.com/api";
+// const BASE_URL = "https://nogaproject.onrender.com/api";
 
 export default axios.create({
   baseURL: BASE_URL,
   headers: {
     "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": "https://nogaproject.onrender.com/api",
   },
   withCredentials: true,
 });
@@ -19,7 +18,6 @@ export const axiosPrivate = axios.create({
   baseURL: BASE_URL,
   headers: {
     "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": "https://nogaproject.onrender.com/api",
   },
   withCredentials: true,
 });
@@ -28,7 +26,6 @@ export const axiosPrivateEmployee = axios.create({
   baseURL: BASE_URL,
   headers: {
     "Content-Type": "multipart/form-data",
-    "Access-Control-Allow-Origin": "https://nogaproject.onrender.com/api",
   },
   withCredentials: true,
 });
