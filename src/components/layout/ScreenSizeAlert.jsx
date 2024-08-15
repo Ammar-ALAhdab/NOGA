a56376e43b3746ce2987bca9053295738990d735
Ammar Al-Ahdab <amar39771@gmail.com>
2024-08-14 17:55:57 -0700

    change BASE url of API for Render Server 5

--- src/components/layout/ScreenSizeAlert.jsx
+++ src/components/layout/ScreenSizeAlert.jsx
@@ -3,11 +3,15 @@ import screen_size from "../../assets/general/screen_size.svg";
 function ScreenSizeAlert() {
   return (
     <main className="flex justify-start items-center pt-20 errBg bg-center z-20 absolute w-full h-[300%] flex-col text-gray-800 gap-4 bg-[#86c2b9]">
-      <img src={screen_size} alt="screen_size" width={400} height={400} />
-      <div className="flex justify-center items-center flex-col gap-2">
-        <h1 className="ar-txt text-5xl mb-2">أووه...لا!</h1>
-        <p className="ar-txt text-2xl">{`من فضلك استخدم شاشة بعرض 800 بكسل أو أعلى،`}</p>
-        <p className="text-2xl">{`.وذلك لتوفير تجربة ملائمة عند استخدام النظام`}</p>
+      <img
+        src={screen_size}
+        alt="screen_size"
+        className="w-[250px] h-[250px] sm:w-[400px] sm:h-[400px]"
+      />
+      <div className="flex justify-center items-center flex-col gap-2 w-full text-center">
+        <h1 className="ar-txt text-3xl sm:text-5xl mb-2">أووه...لا!</h1>
+        <p className="ar-txt text-lg sm:text-2xl">{`من فضلك استخدم شاشة بعرض 800 بكسل أو أعلى،`}</p>
+        <p className=" text-lg sm:text-2xl">{`.وذلك لتوفير تجربة ملائمة عند استخدام النظام`}</p>
       </div>
     </main>
   );
--- src/hooks/useScreenResize.js
+++ src/hooks/useScreenResize.js
@@ -2,12 +2,27 @@ import { useEffect, useState } from "react";
 
 function useScreenResize() {
   const [showScreenAlert, setShowScreenAlert] = useState(false);
-
   useEffect(() => {
     const handleResize = () => {
       setShowScreenAlert(window.innerWidth < 800);
     };
     window.addEventListener("resize", handleResize);
+    if (showScreenAlert) {
+      document.body.style.overflow = "hidden";
+      document.documentElement.scrollTop = 0;
+    } else {
+      document.body.style.overflow = "auto";
+    }
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [showScreenAlert]);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setShowScreenAlert(window.innerWidth < 800);
+    };
+    window.addEventListener("load", handleResize);
     if (showScreenAlert) {
       document.body.style.overflow = "hidden";
       document.documentElement.scrollTop = 0;
